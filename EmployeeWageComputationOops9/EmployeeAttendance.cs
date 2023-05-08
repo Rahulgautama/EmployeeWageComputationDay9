5424288095b4dc236a24c80cbf390e0534c57b74
Rahulgautama <rahul8874950727@gmail.com>
2023-05-08 22:50:46 +0530

    Check Employee Attendance

---
+++ EmployeeWageComputationOops9/EmployeeAttendance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputationOops9
+{
+    class EmployeeAttendance
+    {
+        public int CheckEmpoyeeAttendacne()
+        {          
+            
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            return empCheck;
+        }
+    }
+}
---
+++ EmployeeWageComputationOops9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWageComputationOops9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program using Oops Concept Day9");
+            EmployeeAttendance employeeAttendance = new EmployeeAttendance();
+            int empCheck = employeeAttendance.CheckEmpoyeeAttendacne();
+            const int IS_FULL_TIME = 1;
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Empoyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Empoyee is Absent");
+            }
+        }
+    }
+}
